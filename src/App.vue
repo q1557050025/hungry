29fe7ef784e89c2ac222a1c4728fc749d68e007b
Buke <43104602+q1557050025@users.noreply.github.com>
2019-04-14 21:57:59 +0800

    路由切换动画

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <router-view/>
+    <transition name="router-fade" mode="out-in">
+      <keep-alive>
+        <router-view/>
+      </keep-alive>
+    </transition>
     <svg-icon></svg-icon>
   </div>
 </template>
@@ -14,4 +18,10 @@ export default {
 </script>
 <style lang="scss">
 @import './style/common.scss';
+  .router-fade-enter-active, .router-fade-leave-active {
+    transition: opacity .3s;
+  }
+  .router-fade-enter, .router-fade-leave-active {
+    opacity: 0;
+  }
 </style>
